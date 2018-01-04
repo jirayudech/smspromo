fde0fc7a103710d67f492e48914eef7c0eb150c9
jirayudech <jirayudech@gmail.com>
2018-01-04 10:35:14 +0700

    Initial Commit

---
+++ smspromo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smspromo
+//
+//  Created by Mac mini on 4/1/2561 BE.
+//  Copyright © 2561 RGT Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
